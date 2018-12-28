fd8a6d4704f08f2c43a0af6a6923303203845a99
AngelVzla99 <45955820+AngelVzla99@users.noreply.github.com>
2018-12-27 22:31:58 -0400

    Update grua.py
    Agregada la función de actualizar ultrasonido

--- Robot A/Puente H/grua.py
+++ Robot A/Puente H/grua.py
@@ -1,36 +1,43 @@
+# Faltan los metodos de actualizar los botones
 import pigpio
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+s = []
 
 class Grua(object) :
 	
 	def __init__ (self, pinA, pinB):
-	   
+		self.pinA = pinA
+		self.pinB = pinB
 	    
-	    self.pinA = pinA
-	    self.pinB = pinB
+		self.pi = pigpio.pi()
+		self.pi.set_mode(pinA, pigpio.OUTPUT)
+		self.pi.set_mode(pinB, pigpio.OUTPUT)
 	    
-	    self.pi = pigpio.pi()
-	    self.pi.set_mode(pinA, pigpio.OUTPUT)
-	    self.pi.set_mode(pinB, pigpio.OUTPUT)
-	    self.level = 0
-	    
-	    self.dist = 0
-	    self.swiA = 0
-	    self.swiB = 0
-	    
-	    self.out = 200
+		self.dist = 0
+		self.swiA = true   # Boton inferior
+		self.swiB = true   # Boton superior
+		
+		self.level = 0
+		self.out = 200     # Voltaje que se le pasa al motor
 	    	    
 	def nivel(self, level) :
+		actualizar_ultrasonido()
 		if level == 1:
 			while not swiA :
 				bajar()
 		elif level == 2:
 			while dist >= 7:
 				subir()
+				actualizar_ultrasonido()
 			while dist < 7:
 				bajar()
+				actualizar_ultrasonido()
 		elif level == 3:
 			while dist >= 7:
 				subir()
+				actualizar_ultrasonido()
 		else:
 			while not swiB :
 				subir()
@@ -47,6 +54,13 @@ class Grua(object) :
 	def parar(self):
 		self.pi.set_PWM_dutycycle(self.pinA, 0)
 		self.pi.set_PWM_dutycycle(self.pinB, 0)
+		
+	def actualizar_ultrasonido(self):
+		data = ''.join(list(ser.readline())[:-2])
+	    	if data:
+			result = str(ser.readline().strip())
+			s = result.decode('utf-8').split(',')
+			dist = s[len(s)-2] 
 		
             
 				
