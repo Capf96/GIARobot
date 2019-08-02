2caffc9dd437887449b152a9150e4eaa3ff4507e
AngelVzla99 <angelmolina19@yahoo.es>
2019-08-02 14:26:56 -0400

    Comunicacion con arduino

--- Raspberry/arduino_reader.py
+++ Raspberry/arduino_reader.py
@@ -1,22 +1,16 @@
 import serial
-import sys
-import time
+ser = serial.Serial('/dev/ttyACM0', 9600)
+ser.flushInput()
 
-ser = serial.Serial('/dev/ttyACM0', 57600)
-gyro_values = []
-start = time.time()
-while True:
-        result = str(ser.readline().strip())    # read up to one hundred bytes
-        splitted = result.decode('utf-8').split(',')
-        gyro_reading = float(splitted[0])
-        gyro_values.append(gyro_reading)
-        end = time.time()
-        if end - start >= 1800: # En secs
-                print("#DIFF: ",gyro_values[0], gyro_reading)
-                f = open("Results.txt","w+")
-                f.write(gyro_values[0])
-                for i in range(1,len(gyro_values)):
-                        f.write(",{}".format(gyro_values[i]))
-                        f.close()
-                gyro_values.clear()
-                start = time.time()
+class Arduino(object):
+
+	def getQTR (self):
+		data = ('').join(list(ser.readline()))
+		if data:
+			result = str(ser.readline().strip())
+			result = unicode(result, errors = 'replace' )
+			s = result.decode('utf-8').split(' ')
+			for i in range(len(s)):
+				s[i]=int(s[i])
+			self.values = s
+		return s
--- Raspberry/test1.py
+++ Raspberry/test1.py
@@ -1,9 +1,10 @@
 from MotorControl import Motors
+from arduino_reader import Arduino
 
 motores = Motors(17,27) # Pines GPIO17 y GPIO27
+arduino = Arduino()
 
 motores.stop()
 
 while True:
-	v = float(input())
-	motores.setMotorL(v)
+	print arduino.getQTR()
