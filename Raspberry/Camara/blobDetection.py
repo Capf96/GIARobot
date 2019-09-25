67e29d87e71a0b3bd4763bbe173dedc78aba6591
Wilfredo Graterol <solo.wilfredo64@gmail.com>
2019-09-25 16:23:05 -0400

    Starting with contour detection

--- Raspberry/Camara/blobDetection.py
+++ Raspberry/Camara/blobDetection.py
@@ -3,6 +3,7 @@
 # Standard imports
 import cv2
 import argparse
+import imutils
 import numpy as np
 
 ap = argparse.ArgumentParser()
@@ -44,7 +45,31 @@ for (lower, upper, invert, col) in boundaries:
 #im = cv2.imread("pista_bloque2.jpeg", cv2.IMREAD_GRAYSCALE)
 
 im = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(im, (5, 5), 0)
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
+# find contours in the thresholded image
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+# loop over the contours
+for c in cnts:
+	# compute the center of the contour
+	M = cv2.moments(c)
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+ 
+	# draw the contour and center of the shape on the image
+	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+	cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+	cv2.putText(image, "center", (cX - 20, cY - 20),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+ 
+	# show the image
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
+'''
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
 
@@ -55,19 +80,19 @@ params.maxThreshold = 200
 
 # Filter by Area.
 params.filterByArea = True
-params.minArea = 1500
+params.minArea = 0
 
 # Filter by Circularity
 params.filterByCircularity = True
-params.minCircularity = 0.1
+params.minCircularity = 0.0
 
 # Filter by Convexity
 params.filterByConvexity = True
-params.minConvexity = 0.87
+params.minConvexity = 0.0
     
 # Filter by Inertia
 params.filterByInertia = True
-params.minInertiaRatio = 0.01
+params.minInertiaRatio = 0.00
 
 # Create a detector with the parameters
 ver = (cv2.__version__).split('.')
@@ -85,7 +110,9 @@ keypoints = detector.detect(im)
 # the size of the circle corresponds to the size of blob
 
 im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
 # Show blobs
 cv2.imshow("Keypoints", im_with_keypoints)
-cv2.waitKey(0)
+'''
+
+#cv2.imshow("Keypoints", thresh)
+#cv2.waitKey(0)
