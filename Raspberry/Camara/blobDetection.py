8602d5f1636b2cb8f7948b54c4528554a0cc5b3a
Wilfredo Graterol <solo.wilfredo64@gmail.com>
2019-09-26 23:03:23 -0400

    Improved center detection

--- Raspberry/Camara/blobDetection.py
+++ Raspberry/Camara/blobDetection.py
@@ -52,20 +52,20 @@ cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPL
 cnts = imutils.grab_contours(cnts)
 c = max(cnts, key=len, default=[[]])
 
-# compute the center of the contour
+# compute the center of the contourcX = int(M["m10"] / M["m00"])
 M = cv2.moments(c)
 cX = int(M["m10"] / M["m00"])
+'''
 cY = int(M["m01"] / M["m00"])
 
 # draw the contour and center of the shape on the image
 cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
 cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-#cv2.putText(image, "center", (cX - 20, cY - 20),
-#	cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
+'''
+if(250<=cX<=350) : 
+    print(True)
+else :
+    print(False)
 # show the image
 cv2.imshow("Image", image)
 cv2.waitKey(0)
-
-#cv2.imshow("Keypoints", thresh)
-#cv2.waitKey(0)
