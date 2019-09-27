e17c97091e20019367ec8bed72e13cf64d9cfc78
Wilfredo Graterol <solo.wilfredo64@gmail.com>
2019-09-26 22:52:30 -0400

    Filtering largest contour of image

--- Raspberry/Camara/blobDetection.py
+++ Raspberry/Camara/blobDetection.py
@@ -42,77 +42,30 @@ for (lower, upper, invert, col) in boundaries:
         output = cv2.bitwise_and(to_use, to_use, mask = mask)
  
 # Read image
-#im = cv2.imread("pista_bloque2.jpeg", cv2.IMREAD_GRAYSCALE)
 
 im = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(im, (5, 5), 0)
-thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY)[1]
 
 # find contours in the thresholded image
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
+c = max(cnts, key=len, default=[[]])
 
-# loop over the contours
-for c in cnts:
-	# compute the center of the contour
-	M = cv2.moments(c)
-	cX = int(M["m10"] / M["m00"])
-	cY = int(M["m01"] / M["m00"])
- 
-	# draw the contour and center of the shape on the image
-	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-	cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-	cv2.putText(image, "center", (cX - 20, cY - 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
- 
-	# show the image
-	cv2.imshow("Image", image)
-	cv2.waitKey(0)
-'''
-# Setup SimpleBlobDetector parameters.
-params = cv2.SimpleBlobDetector_Params()
+# compute the center of the contour
+M = cv2.moments(c)
+cX = int(M["m10"] / M["m00"])
+cY = int(M["m01"] / M["m00"])
 
-# Change thresholds
-params.minThreshold = 10
-params.maxThreshold = 200
+# draw the contour and center of the shape on the image
+cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+#cv2.putText(image, "center", (cX - 20, cY - 20),
+#	cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 0
-
-# Filter by Circularity
-params.filterByCircularity = True
-params.minCircularity = 0.0
-
-# Filter by Convexity
-params.filterByConvexity = True
-params.minConvexity = 0.0
-    
-# Filter by Inertia
-params.filterByInertia = True
-params.minInertiaRatio = 0.00
-
-# Create a detector with the parameters
-ver = (cv2.__version__).split('.')
-if int(ver[0]) < 3 :
-	detector = cv2.SimpleBlobDetector(params)
-else : 
-	detector = cv2.SimpleBlobDetector_create(params)
-
-
-# Detect blobs.
-keypoints = detector.detect(im)
-
-# Draw detected blobs as red circles.
-# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
-# the size of the circle corresponds to the size of blob
-
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-# Show blobs
-cv2.imshow("Keypoints", im_with_keypoints)
-'''
+# show the image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
 
 #cv2.imshow("Keypoints", thresh)
 #cv2.waitKey(0)
